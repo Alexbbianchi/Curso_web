ef825c43dde19f838aa342cd96dcf731eb08c8a8
Alex Bianchi <alexbbianchi@gmail.com>
2020-03-10 07:03:17 -0300

    Pronto

--- Javascript_Guanabara/Aula14ex/exe019/script.js
+++ Javascript_Guanabara/Aula14ex/exe019/script.js
@@ -4,18 +4,22 @@ let res = document.querySelector('div#res');
 let valores = [];
     
 function isNumero(num){
-    if(Number(num) >= 1 && Number(num) <=100){
-        return true;
-    }else {
-        return false;
-    }
-}    
-function inLista(num, val){
+    if(Number(num) >= 1 && Number(num) <=100) return true;
     
+    return false;
+}    
+function inLista(num, list){
+    if(list.indexOf(Number(num)) != -1) return true;
+    return false;
 }
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)){
-
+        valores.push(Number(num.value));
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item);
+    }else{
+        alert('Valores invalidos ou já encontrados na lista');
     }
 }
